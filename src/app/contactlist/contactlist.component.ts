e4fe4878c1c75a48abd995073d678b9d041633b8
itsdevkr <kdev2817@gmail.com>
2022-12-19 20:24:10 +0530

    contact-list component created

--- src/app/contactlist/contactlist.component.ts
+++ src/app/contactlist/contactlist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { Contact } from '../contact';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contactlist',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./contactlist.component.css']
 })
 export class ContactlistComponent {
+  
+  constructor(private service:ContactService, private router:Router){}
 
+  contacts:Contact[] = [];
+  delMsg:string="";
+
+  getContacts(){
+    this.service.getContacts().subscribe(response => {
+      this.contacts = response;
+    });
+  }
+
+  editContact(id:number){
+    this.router.navigate(['/edit', id]);
+  }
+
+  deleteContact(id:number){
+    this.service.deleteContactById(id).subscribe(response =>{
+      this.delMsg = response;
+    });
+    this.getContacts();
+  }
 }
